400df76f21d69273e08be30b330f59e615e43fd1
Derrick <derrickh4084@gmail.com>
2026-02-05 18:32:00 -0500

    added mongo bean to remove _class column in mongodb

--- src/main/java/com/derocode/customer_grpc_server/configs/MongoClientConfig.java
+++ src/main/java/com/derocode/customer_grpc_server/configs/MongoClientConfig.java
@@ -1,14 +1,20 @@
 package com.derocode.customer_grpc_server.configs;
 
+import com.mongodb.DBRef;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.bson.Document;
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
 
 import java.util.List;
 
@@ -37,5 +43,17 @@ public class MongoClientConfig {
     @Bean
     public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
         return new SimpleMongoClientDatabaseFactory(mongoClient,"customer");
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory factory,
+            MongoCustomConversions conversions,
+            MongoMappingContext context) {
+        MappingMongoConverter mappingMongoConverter = new MappingMongoConverter(new DefaultDbRefResolver(factory), context);
+        mappingMongoConverter.setCustomConversions(conversions);
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return mappingMongoConverter;
+
     }
 }
